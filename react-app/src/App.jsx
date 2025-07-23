6d8a1eaf663509ac1739707296c866962c856cd9
unknown <zenahmad06@gmail.com>
2025-07-22 17:56:23 -0700

    add to repo

---
+++ react-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// src/App.jsx
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>Hello from Vite + React!</h1>
+      <p>This is a simple static app.</p>
+    </div>
+  );
+}
+
+export default App;
+
+
